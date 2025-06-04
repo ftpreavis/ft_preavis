f40297c70298ac8fe1e346e6784bff6b79cb1930
Charles POULAIN <charles.probe@gmail.com>
2025-06-04 12:29:44 +0200

    Fix: Typo in variables

--- Vars.mk
+++ Vars.mk
@@ -6,7 +6,7 @@
 #    By: cpoulain <cpoulain@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/04/14 15:23:07 by cpoulain          #+#    #+#              #
-#    Updated: 2025/06/04 12:23:23 by cpoulain         ###   ########.fr        #
+#    Updated: 2025/06/04 12:29:35 by cpoulain         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -33,7 +33,7 @@ SCRIPT_DIR				:=	$(INFRA_DIR)/scripts
 INIT_ENV_SCRIPT_PATH	:=	$(SCRIPT_DIR)/init_dev_env.sh
 INIT_CSV_SCRIPT_PATH	:=	$(SCRIPT_DIR)/init_env_csv.sh
 DEV_ENV_PATH			:=	$(INFRA_DIR)/env/.dev.env
-CSV_ENV_PATH			:=	$(INFRA)/vault/seeder/.env.csv
+CSV_ENV_PATH			:=	$(INFRA_DIR)/vault/seeder/.env.csv
 
 # Prod volumes
 
